6142e09e37f56f5d56fb7b1babe56b941b319a7b
kitpao <kitpao91@gmail.com>
2020-08-11 17:11:13 -0500

    :sparkles: Add user-votes association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :articles, foreign_key: :author_id, dependent: :destroy
+  has_many :votes, dependent: :destroy
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,4 @@
 class Vote < ApplicationRecord
+  belongs_to :user
+  #unique combination user-post: validates :user_id, uniqueness: { scope: :article_id }
 end
