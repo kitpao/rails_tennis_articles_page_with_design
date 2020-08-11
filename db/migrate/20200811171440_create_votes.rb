d1d1490094d7cdf1c0032513cb12681c68b480d9
kitpao <kitpao91@gmail.com>
2020-08-11 12:38:47 -0500

    :card_file_box: Add columns to votes table

--- db/migrate/20200811171440_create_votes.rb
+++ db/migrate/20200811171440_create_votes.rb
@@ -1,7 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[6.0]
   def change
     create_table :votes do |t|
-
+      t.references :user
+      t.references :article
       t.timestamps
     end
   end
