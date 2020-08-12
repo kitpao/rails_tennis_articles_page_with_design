9b79cf38b05d1a6ae633dbe2b011b80aca78a4ce
kitpao <kitpao91@gmail.com>
2020-08-12 16:41:17 -0500

    :sparkles: Add methods for User#new and User#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
   def new
-    # blank
+    @user = User.new
   end
 
   def create
-    # blank
+    @user = User.new(user_params)
+    @user.name.downcase!
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'User created succesfully'
+      redirect_to root_path
+    else
+      flash.now[:notice] = 'Something went wrong, please check the errors'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
