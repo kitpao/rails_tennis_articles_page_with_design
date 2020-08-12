17602dcbd4f921eca1805d6ec0bd28e20ef355c2
kitpao <kitpao91@gmail.com>
2020-08-11 22:19:04 -0500

    :wrench: Add routes for all the pages and needed urls

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :article
-  # unique combination user-post: validates :user_id, uniqueness: { scope: :article_id }
+
+  validates :user_id, uniqueness: { scope: :article_id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create] #sign up
+  resources :sessions, only: %i[create destroy] #login/logout
+  resources :articles, only: %i[new create] #new form
+  resources :votes, only: %i[create destroy] #vote / unvote
+  resources :categories, only: %i[index show] #homepage / #each category
+
+  root 'categories#index'
 end
