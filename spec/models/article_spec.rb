95d0fef937f45cbf31f0fbe7ce3f04918703edef
kitpao <kitpao91@gmail.com>
2020-08-12 15:56:36 -0500

    :white_check_mark: Add test for Article model associations and validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Article do
+    it 'has associations' do
+      should belong_to(:author).class_name('User')
+      should have_many(:votes)
+      should have_and_belong_to_many(:categories)
+
+    end
+
+    it 'validates' do
+      should validate_presence_of(:title)
+      should validate_uniqueness_of(:title).case_insensitive
+      should validate_presence_of(:text)
+      should validate_presence_of(:categories)
+    end
+  end
+
 end
