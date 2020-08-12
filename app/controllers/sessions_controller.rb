cf3a35c423729f12c4fa0ed41d24fcca73f617cb
kitpao <kitpao91@gmail.com>
2020-08-12 16:51:34 -0500

    :sparkles: Add methods for Sessions#new, Sessions#create and Sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,27 @@
 class SessionsController < ApplicationController
-  def new
-    # blank
-  end
+  def new; end
 
   def create
-    # blank
+    user = User.find_by_name(user_session_params.downcase)
+    if user && !user.nil?
+      session[:user_id] = user.id
+      flash[:success] = 'You have logged in!'
+      redirect_to root_path
+    else
+      flash[:notice] = 'Account doesn\'t exist, please sign up'
+      redirect_to sign_up_path
+    end
   end
 
   def destroy
-    # blank
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out'
+    redirect_to root_path
+  end
+
+  private
+
+  def user_session_params
+    params.require(:session).permit(:name)
   end
 end
