0411aa056264d1462d9de8fb86f46f76c3da0f46
kitpao <kitpao91@gmail.com>
2020-08-12 16:16:38 -0500

    :white_check_mark: Add test for Vote model associations and validations

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Vote do
+    it 'has associations' do
+      should belong_to(:user)
+      should belong_to(:article)
+    end
+
+    it 'validates' do
+      should validate_uniqueness_of(:user_id).scoped_to(:article_id)
+    end
+  end
 end
