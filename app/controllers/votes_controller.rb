be96cfe21c1d4dfac7662179e0f631639f7891f7
kitpao <kitpao91@gmail.com>
2020-08-13 14:39:07 -0500

    :sparkles: Add methods for Vote#create and Vote#destroy

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,21 @@
 class VotesController < ApplicationController
   def create
-    # blank
+    category_id = params[:id] # take the id from current url CHECK THIS
+    @vote = current_user.votes.new(article_id: params[article_id])
+    if @vote.save
+      redirect_to category_path(category_id), notice: 'You voted for an article.'
+    else
+      redirect_to category_path(category_id), alert: 'There was a problem, please try again.'
+    end
   end
 
   def destroy
-    # blank
+    vote = Vote.find_by(user: current_user, article_id: params[:article_id])
+    if vote
+      vote.destroy
+      redirect_to posts_path, notice: 'You unvoted an article.'
+    else
+      redirect_to posts_path, alert: 'There was a problem, please try again.'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[create] # sign up down
   resources :sessions, only: %i[create] # login/logout down
-  resources :articles, only: %i[new create] # new form
-  resources :votes, only: %i[create destroy] # vote / unvote
+  resources :articles, only: %i[new create] do # new form
+    resources :votes, only: %i[create destroy] # vote / unvote
+  end
   resources :categories, only: %i[show] # homepage down / #each category
   get '/sign_up', to: 'users#new' # sign up
   get '/log_in', to: 'sessions#new' # log in
