57e33d3448dea637afb6d57dcc6811397d4742c3
kitpao <kitpao91@gmail.com>
2020-08-12 23:31:14 -0500

    :sparkles: Add helper methods for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :sign_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def sign_in?
+    current_user ? true : false
+  end
+
+  def require_user
+    return if sign_in?
+
+    flash[:danger] = 'You must be logged in to perform this action'
+    redirect_to root_path
+  end
 end
