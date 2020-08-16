ef36eb79157a97b9e6229da14d36cf99078825ee
kitpao <kitpao91@gmail.com>
2020-08-15 23:10:39 -0500

    :lipstick: Add almost all text for categories#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,6 +45,8 @@ module ApplicationHelper
 
   def display_details(art)
     content_tag(:h1, @category.name, class: 'orange-text') +
-      content_tag(:h2, art.title)
+      content_tag(:h2, art.title) +
+      content_tag(:p, simple_format(art.text.truncate(400))) +
+      content_tag(:span, "#{art.votes_count} votes")
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
   has_and_belongs_to_many :categories
   has_one_attached :image
 
-  validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 60 }
   validates :text, presence: true
   # add a default image
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
