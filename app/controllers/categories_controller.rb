de39101ae5093702493e96eeeb348c0dc0cbf76f
kitpao <kitpao91@gmail.com>
2020-08-18 17:42:41 +0000

    :fire: delete commented code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  # add before filter to create categories and 1 article per category
+
   def index
     @categories = Category.includes(:articles).all
     articles = Article.all
@@ -25,22 +25,3 @@ class CategoriesController < ApplicationController
     category.articles.ordered_by_most_recent.first
   end
 end
-
-# for images in views, use <%= url_for(@article.image) %>
-
-# <%= cl_image_tag(@user.avatar.key, width: 200, crop: :scale) %>
-
-# OR
-
-# <%= cloudinary_url(@user.avatar.key, options = {}) %>
-
-# if @article.image.attached?
-# their image
-# else
-# default -> user.avatar.attach(params[:avatar]) MAYBE, I may have to reset the previos image, check more
-
-# https://edgeguides.rubyonrails.org/active_storage_overview.html
-
-# https://hackernoon.com/image-storage-in-rails-apps-using-cloudinary-and-active-storage-9w2u3yli
-
-# https://hackernoon.com/image-storage-in-rails-apps-using-cloudinary-and-active-storage-9w2u3yli
